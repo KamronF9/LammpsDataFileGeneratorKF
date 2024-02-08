3ca82fa0c9d7c79506ef96c17f512a1d20d02f46
Kamron Fazel <kamron@login34.chn.perlmutter.nersc.gov>
2024-02-08 09:31:41 -0800

    plot added

---
+++ analyzePolyPlot.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+# Python 3.10.4
+# Distributed under the terms of the MIT License.
+# If you make an update that you feel others would need, feel free to make
+# a merge request to the main branch with your update
+# (https://github.com/cpueschel/Lammps-Data-File-Generator.git).
+
+# dpdata1 local env
+#numpy                     1.23.3                   pypi_0    pypi
+#pymatgen                  2023.1.30                pypi_0    pypi
+
+#PM env deepmd3
+
+#TODO  add read in of structure from poscar instead of config
+
+from __future__ import print_function
+import numpy as np
+from numpy import linalg as LA
+import math
+# from pymatgen.io.xyz import XYZ
+# from pymatgen.core import structure
+# from pymatgen.io.lammps import outputs
+# from pymatgen.core.sites import PeriodicSite
+# from pymatgen.core import Site
+# from pymatgen.core import SETTINGS, Element, Lattice, Structure
+# from pymatgen import Lattice, Structure, Molecule
+import yaml
+# from pymatgen.io.vasp import Poscar, sets
+from string import digits
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# [H_PtTotal,O_PtTotal,O_OTotal,O_HTotal] # append to df
+
+df = pd.read_csv('data.csv')
+
+print(df)
+
+plt.figure(figsize=(10,7), dpi=300)
+
+for i,col in enumerate(df.columns):
+    # if i==1:continue
+    plt.plot(df[col], label=col)
+
+plt.xlabel('100fs interval')
+plt.ylabel('#')
+plt.legend()
+plt.grid(True)
+
+plt.savefig('test.png')
